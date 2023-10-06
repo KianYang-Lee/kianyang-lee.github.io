809775706305db04a6a3a998df4112cc8301e856
kianyang_lee <ken.lee.kianyang@gmail.com>
2023-10-06 13:07:43 +0800

    Correctly sized icons for small screens

--- src/lib/components/Icon.svelte
+++ src/lib/components/Icon.svelte
@@ -3,18 +3,16 @@
 	export let tooltipData: string;
 </script>
 
-<div class="group relative w-max">
-	<a
-		href={url}
-		target="_blank"
-		rel="noreferrer"
-		class="sm:inline-flex items-center justify-center w-10 h-10 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1"
+<a
+	href={url}
+	target="_blank"
+	rel="noreferrer"
+	class="group relative sm:inline-flex items-center justify-center w-10 h-10 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1"
+>
+	<slot>fallback</slot>
+	<span class="sr-only">{tooltipData}</span>
+	<span
+		class="bg-black text-white font-semibold p-2 pointer-events-none lg:absolute -top-8 lg:-left-8 w-max opacity-0 transition-opacity group-hover:opacity-100"
+		>{tooltipData}</span
 	>
-		<slot>fallback</slot>
-		<span class="sr-only">{tooltipData}</span>
-		<span
-			class="bg-black text-white font-semibold p-2 pointer-events-none absolute -top-8 -left-8 w-max opacity-0 transition-opacity group-hover:opacity-100"
-			>{tooltipData}</span
-		>
-	</a>
-</div>
+</a>
