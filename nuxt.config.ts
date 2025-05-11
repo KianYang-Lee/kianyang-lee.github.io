4e8dd6eb5bad66cc4604bb15daa4e1b683113fdb
KianYang-Lee <ken.lee.kianyang@gmail.com>
2025-05-11 14:18:17 +0800

    add gtag with existing ID

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/eslint', '@nuxt/ui']
-})
+    compatibilityDate: '2024-11-01',
+    devtools: { enabled: true },
+    modules: ['@nuxt/content', '@nuxt/eslint', '@nuxt/ui', 'nuxt-gtag'],
+    gtag: {
+        id: 'G-2898RVF3GV',
+    },
+});
+
